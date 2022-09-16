1005f3aedefa09bb8057e42a94f32c050caab6cf
padmitriev <47365954+padmitriev@users.noreply.github.com>
2022-09-16 10:08:57 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,65 +1,36 @@
-import java.util.ArrayList;
-import java.util.Random;
+import java.lang.reflect.InvocationTargetException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        System.out.println(getRandomName());
-//        checkIfTheWorldIsNotEmpty();
+// THeWorld:
+//        TheWorld theWorld = new TheWorld();
+//        int[] theNewWorld;
+//        System.out.println(Arrays.toString(theWorld.getTheWorld()));
+//
+//        theNewWorld = ManipulatingTools.moveObject(theWorld.getTheWorld(), false);
+//        System.out.println(Arrays.toString(theNewWorld));
+//
+//        CommandControls.invokeMethod();
 
-//        int[] theWorld0 = TheWorld.getTheWorld();
-//        ObservingTools gfjgf = new ObservingTools();
-//        ArrayList<Integer> vhf = gfjgf.findObject(theWorld0);
-//        System.out.println("----------------------");
-//        for (int gdfg : vhf) {
-//            System.out.println(gdfg);
-//        }
+//        ObservingTools.listOfMethods(Class.forName("main.java.ObservingTools"));
 
-        ManipulatingTools wfjhfg = new ManipulatingTools();
-        wfjhfg.moveLeft();
+//        ____________________________________________
+// API:
+//        String uriForTest = "http://jsonplaceholder.typicode.com/posts";
 
+//        httpRequests.sendGet(uriForTest);
 
+//        List<NameValuePair> params = new ArrayList<>();
+//        params.add(new BasicNameValuePair("title", "foo"));
+//        params.add(new BasicNameValuePair("body", "bar"));
+//        params.add(new BasicNameValuePair("userId", "1"));
+//
+//        httpRequests.sendPost(uriForTest, params);
 
-
+//        ______________________________________________
+// Desktop:
+//        ObservingTools.getScreenshot();
 
     }
-
-    private static void checkIfTheWorldIsNotEmpty() {
-        int zero = 0;
-        ArrayList<Integer> object = new ArrayList<>();
-        for (int worldElement: getTheWorld()) {
-            if (worldElement != zero) {
-                object.add(worldElement);
-            }
-        }
-
-        int[] newObject = new int[object.size()];
-
-        for (int i = 0; i < object.size(); i++) {
-            newObject[i] = object.get(i);
-        }
-        System.out.println(newObject.length);
-    }
-
-    public static String getRandomName() {
-        char nextChar;
-        StringBuilder sb = new StringBuilder();
-        Random rnd = new Random();
-
-        for(int i = 0; i < 3; i++) {
-            nextChar = (char) (rnd.nextInt(26) + 97);
-            sb.append(nextChar);
-        }
-
-        return sb.toString();
-    }
-
-    private static int[] getTheWorld() {
-        int[] theWorld = {0, 0, 1, 1, 1, 0, 0};
-        return theWorld;
-    }
-
-
-
-
 }
