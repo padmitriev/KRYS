8955f415c64959bc03c41f41fd7aac3fa656f3df
padmitriev <47365954+padmitriev@users.noreply.github.com>
2022-09-16 10:11:19 +0300

    Update TheWorld.java

--- src/main/java/TheWorld.java
+++ src/main/java/TheWorld.java
@@ -1,41 +1,19 @@
+
 public class TheWorld {
-
     private static TheWorld instance;
-    private static int[] theWorld = {0, 0, 1, 1, 1, 0, 0};
+    private int[] theWorld = {0, 0, 1, 1, 1, 0, 0};
 
-    private TheWorld() {
+    TheWorld() {
     }
 
     public static TheWorld getInstance(){
-        if(instance == null){		//если объект еще не создан
-            instance = new TheWorld();	//создать новый объект
+        if(instance == null){
+            instance = new TheWorld();
         }
         return instance;
-
     }
 
-    public static int[] getTheWorld() {
+    public int[] getTheWorld() {
         return theWorld;
     }
-
-    public static void setTheWorld(int[] theWorld) {
-        TheWorld.theWorld = theWorld;
-    }
-
-
-
-//    private static LazyInitializedSingleton instance; //#1
-//
-//    private LazyInitializedSingleton() {
-//    }
-//
-//    public static LazyInitializedSingleton getInstance(){ // #3
-//        if(instance == null){		//если объект еще не создан
-//            instance = new LazyInitializedSingleton();	//создать новый объект
-//        }
-//        return instance;		// вернуть ранее созданный объект
-//    }
-
-
-
 }
